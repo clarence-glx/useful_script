2b6e9602cf73ae0f4b8b93f946d6eff5e0483211
Clarence <mcfarland.glx@foxmail.com>
2023-02-28 23:03:31 +0800

    update build_llvm.sh

--- build_llvm.sh
+++ build_llvm.sh
@@ -5,29 +5,30 @@ C_COMPILER_PATH="/usr/bin/clang"
 CXX_COMPILER_PATH="/usr/bin/clang++"
 LLVM_ENABLE_PROJECTS="clang"
 
-LLVM15_DOWNLOAD_LINK="https://codeload.github.com/llvm/llvm-project/tar.gz/refs/tags/llvmorg-15.0.5"
-LLVM15_SRC=$ROOT/llvm-project-llvmorg-15.0.5
+declare -r LLVM_TAG="llvmorg-15.0.7"
+LLVM_DOWNLOAD_LINK="https://codeload.github.com/llvm/llvm-project/tar.gz/refs/tags/$LLVM_TAG"
+declare -r LLVM_SRC=$ROOT/llvm-project-$LLVM_TAG
 
 function download_llvm
 {
-    if [[ ! -f $ROOT/llvm-project-llvmorg-15.0.5.tar.gz ]]; then
+    if [[ ! -f $LLVM_SRC.tar.gz ]]; then
 	echo "starting downloading llvm..."
-	wget -O llvm-project-llvmorg-15.0.5.tar.gz $LLVM15_DOWNLOAD_LINK
+	wget -O llvm-project-llvmorg-$LLVM_TAG.tar.gz $LLVM_DOWNLOAD_LINK
     fi
 }
 
 function build_llvm
 {
-    if [[ ! -d $LLVM15_SRC ]]; then
-	tar -xf $LLVM15_SRC.tar.gz
+    if [[ ! -d $LLVM_SRC ]]; then
+	tar -xf $LLVM_SRC.tar.gz
     fi
-    cd $LLVM15_SRC
+    cd $LLVM_SRC
     if [[ -d build ]]; then
 	rm -rf  build
     fi
     mkdir -p build && cd build
     cmake -G "Ninja" \
-	  -DCMAKE_INSTALL_PREFIX="$ROOT/llvm15_install" \
+	  -DCMAKE_INSTALL_PREFIX="$ROOT/llvm_install" \
 	  -DLLVM_USE_LINKER=lld \
 	  -DLLVM_ENABLE_PROJECTS=$LLVM_ENABLE_PROJECTS \
 	  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
@@ -38,7 +39,7 @@ function build_llvm
 	  -DCMAKE_CXX_COMPILER=$CXX_COMPILER_PATH \
 	  -DCMAKE_C_COMPILER=$C_COMPILER_PATH \
 	  ../llvm
-    ninja all || exit 1 
+    ninja all || exit 1
     ninja install || exit 1
 }
 
@@ -48,4 +49,4 @@ function main
     build_llvm
 }
 
-main
+main $@
