debad12f7eea34f0f0462141a4da3b973833a02c
clarence <mcfarland.glx@icloud.com>
2023-04-20 23:38:03 +0800

    update

--- build-compiler.sh
+++ build-compiler.sh
@@ -1,57 +1,33 @@
-#!/usr/bin/bash
+#!/usr/bin/env zsh
 # compile llvm and gcc
 # ./build-compiler.sh <llvm15/gcc7/gcc8/gcc9> <debug/release/rel(only for llvm)>
 
 ROOT_PATH=$(pwd)
-GCC7_SRC=$ROOT_PATH/gcc-7.3.0
-GCC8_SRC=$ROOT_PATH/gcc-8.3.0
-GCC9_SRC=$ROOT_PATH/gcc-9.3.0
-LLVM15_SRC=$ROOT_PATH/llvm-project-llvmorg-15.0.5
+GCC7_SRC=${ROOT_PATH}/gcc-7.3.0
+GCC8_SRC=${ROOT_PATH}/gcc-8.3.0
+GCC9_SRC=${ROOT_PATH}/gcc-9.3.0
+GCC10_SRC=${ROOT_PATH}/gcc-10.3.0
+
+LLVM15_SRC=${ROOT_PATH}/llvm-project-llvmorg-15.0.5
 
 LLVM15_DOWNLOAD_LINK="https://codeload.github.com/llvm/llvm-project/tar.gz/refs/tags/llvmorg-15.0.5"
 GCC7_DOWNLOAD_LINK="https://mirrors.sjtug.sjtu.edu.cn/gnu/gcc/gcc-7.3.0/gcc-7.3.0.tar.gz"
 GCC8_DOWNLOAD_LINK="https://mirrors.sjtug.sjtu.edu.cn/gnu/gcc/gcc-8.3.0/gcc-8.3.0.tar.gz"
 GCC9_DOWNLOAD_LINK="https://mirrors.sjtug.sjtu.edu.cn/gnu/gcc/gcc-9.3.0/gcc-9.3.0.tar.gz"
+GCC10_DOWNLOAD_LINK="https://mirrors.sjtug.sjtu.edu.cn/gnu/gcc/gcc-10.3.0/gcc-10.3.0.tar.gz"
 
-function check_file
+function download_compiler
 {
-    echo $1
-    if [[ $1 == "llvm15" ]]; then
-	if [[ ! -f $LLVM15_SRC.tar.gz ]]; then
-	    wget -O $LLVM15_SRC.tar.gz $LLVM15_DOWNLOAD_LINK
-	    if [[ $? -ne 0 ]]; then
-		echo "download $LLVM15_DOWNLOAD_LINK failed."
-		exit 1
-	    fi
+    declare -u upper_case="$1"
+
+    if [[ ! -f ${upper_case}_SRC.tar.gz ]]; then
+	wget -O ${upper_case}_SRC.tar.gz ${${upper_case}_DOWNLOAD_LINK}
+	if [[ $? -ne 0 ]]; then
+	    echo "download ${${upper_case}_DOWNLOAD_LINK} failed."
+	    return 1
 	fi
     fi
-    if [[ $1 == "gcc7" ]]; then
-	if [[ ! -f $GCC7_SRC.tar.gz ]]; then
-	    wget -O $GCC7_SRC.tar.gz $GCC7_DOWNLOAD_LINK
-	    if [[ $? ]]; then
-		echo "download $GCC7_DOWNLOAD_LINK failed."
-		exit 1
-	    fi
-	fi
-    fi
-    if [[ $1 == "gcc8" ]]; then
-	if [[ ! -f $GCC8_SRC.tar.gz ]]; then
-	    wget -O $GCC8_SRC.tar.gz $GCC8_DOWNLOAD_LINK
-	    if [[ $? ]]; then
-		echo "download $GCC8_DOWNLOAD_LINK failed."
-		exit 1
-	    fi
-	fi
-    fi
-    if [[ $1 == "gcc9" ]]; then
-	if [[ ! -f $GCC9_SRC.tar.gz ]]; then
-	    wget -O $GCC8_SRC.tar.gz $GCC9_DOWNLOAD_LINK
-	    if [[ $? ]]; then
-		echo "download $GCC9_DOWNLOAD_LINK failed."
-		exit 1
-	    fi
-	fi
-    fi
+    return 0
 }
 
 function check_del_tar_dir
@@ -71,7 +47,7 @@ function build_gcc_common
 function build_gcc7
 {
     echo "build_gcc7 $1"
-    check_file gcc7
+    download_compiler gcc7 || return 1
     check_del_tar_dir $GCC7_SRC
     
 }
@@ -79,21 +55,21 @@ function build_gcc7
 function build_gcc8
 {
     echo "build_gcc8 $1"
-    check_file gcc8
+    download_compiler gcc8
     check_del_tar_dir $GCC8_SRC
 }
 
 function build_gcc9
 {
     echo "build_gcc9 $1"
-    check_file gcc9
+    download_compiler gcc9
     check_del_tar_dir $GCC9_SRC
 }
 
 function build_llvm15
 {
     echo "build_llvm15 $1"
-    check_file llvm15
+    download_compiler llvm15
     check_del_tar_dir $LLVM15_SRC
     tar -xf $LLVM15_SRC.tar.gz
     rm -rf ./build
